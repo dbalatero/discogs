41e0a72c476bf58381c63fea060aeb36ea51fcf7
David Balatero <dbalatero@gmail.com>
2017-01-06 11:43:18 -0800

    Create script

---
+++ create.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'pp'
+require 'cgi'
+
+releases = JSON.parse(File.read("releases.json"))
+
+def artist_links(artists)
+  artists.map do |artist|
+    %Q{<a href="#{artist['resource_url']}" target="_blank">#{artist['name']}</a>}
+  end.join(", ")
+end
+
+def notes_li(notes)
+  notes.map do |note|
+    name = {
+      1 => "Disc Condition",
+      2 => "Sleeve Condition",
+      3 => "Extra Notes"
+    }[note['field_id']]
+
+    %Q{<li class="note"><span class="note-name">#{name}:</span> #{note['value']}</li>}
+  end.join("\n")
+end
+
+def release_html(release)
+  info = release['basic_information']
+
+  <<-EOF
+  <div class="release">
+    <div class="thumbnail">
+      <a href="#{info['resource_url']}" target="_blank">
+        <img src="#{info['thumb']}" />
+      </a>
+    </div>
+    <div class="info">
+      <div class="artist-title">
+        <span class="artists">#{artist_links(info['artists'])}</span>
+        &mdash;
+        <a href="#{info['resource_url']}" target="_blank" class="title">#{info['title']}</a>
+      </div>
+
+      <ul class="info">
+        #{notes_li(release['notes'])}
+      </ul>
+
+      <a class="youtube" href="https://www.youtube.com/results?search_query=#{CGI.escape(info['title'])}" target="_blank">Search Youtube</a>
+    </div>
+  </div>
+  EOF
+end
+
+puts release_html(releases[0])
+
+puts "-------"
+
+pp releases.first
