22d610b844ccdec8510396155a80b8aae6793663
David Balatero <dbalatero@gmail.com>
2017-01-06 12:11:17 -0800

    First pass

--- create.rb
+++ create.rb
@@ -28,31 +28,31 @@ def release_html(release)
   info = release['basic_information']
 
   <<-EOF
-  <div class="release">
-    <div class="thumbnail">
-      <a href="#{info['resource_url']}" target="_blank">
-        <img src="#{info['thumb']}" />
-      </a>
-    </div>
-    <div class="info">
-      <div class="artist-title">
-        <span class="artists">#{artist_links(info['artists'])}</span>
-        &mdash;
-        <a href="#{info['resource_url']}" target="_blank" class="title">#{info['title']}</a>
+    <div class="release">
+      <div class="thumbnail">
+        <a href="#{info['resource_url']}" target="_blank">
+          <img src="#{info['thumb']}" />
+        </a>
       </div>
+      <div class="info">
+        <div class="artist-title">
+          <span class="artists">#{artist_links(info['artists'])}</span>
+          &mdash;
+          <a href="#{info['resource_url']}" target="_blank" class="title">#{info['title']}</a>
+        </div>
 
-      <ul class="info">
-        #{notes_li(release['notes'])}
-      </ul>
+        <ul class="info">
+          #{notes_li(release['notes'])}
+        </ul>
 
-      <a class="youtube" href="https://www.youtube.com/results?search_query=#{CGI.escape(info['title'])}" target="_blank">Search Youtube</a>
+        <a class="youtube" href="https://www.youtube.com/results?search_query=#{CGI.escape(info['title'])}" target="_blank">Search Youtube</a>
+      </div>
     </div>
-  </div>
   EOF
 end
 
-puts release_html(releases[0])
+html = releases.map do |release|
+  release_html(release)
+end.join("\n\n")
 
-puts "-------"
-
-pp releases.first
+puts html
